680353b042dd1bb0ce7cc3546baf8d6fa8868858
Dimitriy ay0ks <dimitriy.ga@gmail.com>
2022-08-06 17:15:58 +0200

    Add a generated example

--- test.asm
+++ test.asm
@@ -2,61 +2,61 @@
 	_using $:lua
 	_using $:sol
 .data
-		L#None := sol::state
-		test_var#None := 10
+		L := sol::state
+		test_var := 10
 .text
 		L:open_lib(sol::lib::base, sol::lib::package)
 		i := 0
-		jmp _EKl465Y1Hqk#u#GRTRAY
-:_EKl465Y1Hqk#u#GRTRAY
-		jmp:neq!(i, math:fac(999)) _NF2DwrRPOzKVSCIUhW$g
-		jmp _iz6PVOZs$iznQ3$I1YXd
-:_NF2DwrRPOzKVSCIUhW$g
+		jmp _LCY3$Y@$liDyzT8#doXZd
+:_LCY3$Y@$liDyzT8#doXZd
+		jmp:neq!(i, math:fac(999)) _D3aJG9oj$EThQ8P7VOGc0
+		jmp _5eIJVA4YsEmPjqsWBcUvg
+:_D3aJG9oj$EThQ8P7VOGc0
 		_printf <u8>'%d left':fmt!(test_var)
 		add! i, 1
-		jmp _EKl465Y1Hqk#u#GRTRAY
-:_iz6PVOZs$iznQ3$I1YXd
+		jmp _LCY3$Y@$liDyzT8#doXZd
+:_5eIJVA4YsEmPjqsWBcUvg
 		del i
 		cx := 0
-		jmp _Myx@@R9#3iL1ji#uQTT@
-:_Myx@@R9#3iL1ji#uQTT@
-		jmp:neq!(cx, math:fac(999)) _63WMRLe$$H@twAENyCIv
-		jmp _AX4ia$@$VAr2rc402hML
-:_63WMRLe$$H@twAENyCIv
+		jmp _Y$#@Ev6E6pRFLZNNxt9cS
+:_Y$#@Ev6E6pRFLZNNxt9cS
+		jmp:neq!(cx, math:fac(999)) _8JJ5@2iRZ#BHy63dw@gPG
+		jmp _eYcw$YDHVQWA#F@lNBQK1
+:_8JJ5@2iRZ#BHy63dw@gPG
 		_printf <u16>'%d манул':fmt!(test_var, (test_var:size(), leq! 4) ? 'а' : 'ов')
 		add! cx, 1
-		jmp _Myx@@R9#3iL1ji#uQTT@
-:_AX4ia$@$VAr2rc402hML
+		jmp _Y$#@Ev6E6pRFLZNNxt9cS
+:_eYcw$YDHVQWA#F@lNBQK1
 		del cx
 		dx := 0
-		jmp _UWFWL7wtDIpLH8Uox@o2
-:_UWFWL7wtDIpLH8Uox@o2
-		jmp:neq!(dx, math:fac(999)) _h#Ikj#cdsKJsOVs4Bv9$
-		jmp _w#HN3UMEDrsXYPLk$WNF
-:_h#Ikj#cdsKJsOVs4Bv9$
+		jmp _BPI0l$2mcDVNseUXUTyZR
+:_BPI0l$2mcDVNseUXUTyZR
+		jmp:neq!(dx, math:fac(999)) _c$WxFhppJ$Ke5ysQn7bYR
+		jmp _TWIFBOP$QC9u2erYCLb4L
+:_c$WxFhppJ$Ke5ysQn7bYR
 		_printf <u16>'%d манул(а/ов)':fmt!(test_var)
 		add! dx, 1
-		jmp _UWFWL7wtDIpLH8Uox@o2
-:_w#HN3UMEDrsXYPLk$WNF
+		jmp _BPI0l$2mcDVNseUXUTyZR
+:_TWIFBOP$QC9u2erYCLb4L
 		del dx
-		wxA5CDPSnIG7xoj$tuJh := enumerate(L:get_states())
-		ikV#PSQDKVIScC03fTeWe#i8 = 0
-		jmp _Gi$96@2IKKDHmY3SpTam
-:_Gi$96@2IKKDHmY3SpTam
-		jmp:neq!(_ikV#PSQDKVIScC03fTeWe, wxA5CDPSnIG7xoj$tuJh:size()) _ZT9lioYPSeYnL5LRp1qa
-		jmp _#eRLq2vhC$KmJyBPx$uC
-:_ZT9lioYPSeYnL5LRp1qa
-		(el1, el2) := wxA5CDPSnIG7xoj$tuJh[ikV#PSQDKVIScC03fTeWe]
+		BBIzFzqCxIqR8AeSCrbRV := enumerate(L:get_states())
+		i$$UzVR$XGejNpyd3oD@u$#i8 = 0
+		jmp _HCeJ8AfKSifjeO#YXDv#X
+:_HCeJ8AfKSifjeO#YXDv#X
+		jmp:neq!(_i$$UzVR$XGejNpyd3oD@u$, BBIzFzqCxIqR8AeSCrbRV:size()) _VP$rWdSrBeNOEDed5rymc
+		jmp _NjB@$B9CTZGSQjC#VXZlc
+:_VP$rWdSrBeNOEDed5rymc
+		(el1, el2) := BBIzFzqCxIqR8AeSCrbRV[i$$UzVR$XGejNpyd3oD@u$]
 		_printf <u16>'%s %s':fmt!(el1, el2)
-		add! ikV#PSQDKVIScC03fTeWe, 1
-		jmp _Gi$96@2IKKDHmY3SpTam
-:_#eRLq2vhC$KmJyBPx$uC
+		add! i$$UzVR$XGejNpyd3oD@u$, 1
+		jmp _HCeJ8AfKSifjeO#YXDv#X
+:_NjB@$B9CTZGSQjC#VXZlc
 		del (el1, el2)
-		defun test, _test$ZQldNAiY@gu@#yKRUUwv
+		defun test, _test$#81D0mEiCM@36e8WMZ5Qo
 		nop!
-:_test$ZQldNAiY@gu@#yKRUUwv
-		(arg1#<member 'U8' of 'Type' objects>, arg2#<member 'U16' of 'Type' objects>) = ('test', 'тест')
-		(arg1#<member 'U8' of 'Type' objects>, arg2#<member 'U16' of 'Type' objects>) = any!(%*) ?: %1..2
+:_test$#81D0mEiCM@36e8WMZ5Qo
+		(arg1#u8, arg2#u16) = ('test', 'тест')
+		(arg1#u8, arg2#u16) = any!(%*) ?: %1..2
 		_printf <u8>'test'
-		jmp _test$MI1uzMBfE7QD#mBFIrWI
-:_test$MI1uzMBfE7QD#mBFIrWI
+		jmp _test$9C$9xD$KoLd$iWWx#I3UI
+:_test$9C$9xD$KoLd$iWWx#I3UI
